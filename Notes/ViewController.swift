99387ba9027f8678d49d80bcd29a2efff0638fcd
Artem <artemkononov2000@gmail.com>
2021-09-28 13:55:47 +0700

    Initial Commit

---
+++ Notes/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Notes
+//
+//  Created by Артём on 28.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray6
+    }
+}
+
